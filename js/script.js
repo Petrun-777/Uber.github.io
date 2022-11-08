8161d90935b4175cec39e01b919f0a7a6497ec46
Petrun Dmytro <petrun.dmytro@gmail.com>
2022-11-08 11:12:24 +0200

    add new media styles

---
+++ js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu-1'),
+    menuItem = document.querySelectorAll('.menu-item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger-active');
+        menu.classList.toggle('menu-1-active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger-active');
+            menu.classList.toggle('menu-1-active');
+        })
+    })
+})
